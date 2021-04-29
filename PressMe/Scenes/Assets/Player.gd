a3ff914ab98ac0bc75f018a9d011c50088dd7f77
Gellifree <forge.delfadortitan@gmail.com>
2021-04-29 17:01:16 +0200

    Mérföldkövek megetrvezése

--- PressMe/Scenes/Assets/Player.gd
+++ PressMe/Scenes/Assets/Player.gd
@@ -1,21 +1,24 @@
 extends KinematicBody2D
 
-const MOVE_SPEED = 250
-var Direction = "Left"
+var MOVE_SPEED = 250
 
-
-onready var raycast = $RayCast2D
 
 func _ready():
 	pass
 
+func _process(delta):
+	pass
 
 func _physics_process(delta):
-	var direction = Vector2()
+	var direction = Vector2() ## x, y
 	
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
+		
 		$AnimatedSprite.play("Run")
+		
+		
+		
 	if Input.is_action_pressed("move_down"):
 		direction.y += 1
 		$AnimatedSprite.play("Run")
@@ -29,9 +32,16 @@ func _physics_process(delta):
 		direction.x -= 1
 		$AnimatedSprite.set_flip_h(true)
 		$AnimatedSprite.play("Run")
+		
+		
+		
 	direction = direction.normalized()
 	
 	move_and_collide(direction * MOVE_SPEED * delta)
 	
+	
+	#move_and_slide(direction * MOVE_SPEED * delta)
+	
 	if(direction == Vector2.ZERO):
 		$AnimatedSprite.play("Idle")
+		pass
